f095f12e8539c9e4f16ee102b970fc6800e0855b
Athilathesni <usmanchusman606@gmail.com>
2024-12-16 16:49:10 +0530

    files added

--- Cruduser/serverside/connection.js
+++ Cruduser/serverside/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 export default async function connection(){
-    const db=await mongoose.connect('mongodb://127.0.0.1:27017/crud');
+    const db=await mongoose.connect('mongodb+srv://dayisal865:E3ETcUZvFC2wVYoG@cluster0.jpz3o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/crud');
     console.log("database connected");
     
     return db
